73925019b742890f848b9798d52078abbeabc268
Hariraam Kethisvarasuthan <62831820+hraamk@users.noreply.github.com>
2024-11-01 14:29:03 +0530

    Initial commit

---
+++ src/main/java/com/ticketing/simulation/ticket_pool_simulation/TicketPoolSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.simulation.ticket_pool_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPoolSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPoolSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketing/simulation/ticket_pool_simulation/TicketPoolSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing.simulation.ticket_pool_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPoolSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
