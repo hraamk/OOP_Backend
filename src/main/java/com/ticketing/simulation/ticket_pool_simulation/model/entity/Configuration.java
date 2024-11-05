96e58a27dc8fe9080b59aa09fa883d71c057a46b
Hariraam Kethisvarasuthan <62831820+hraamk@users.noreply.github.com>
2024-11-06 00:43:50 +0530

    Created Classes

---
+++ src/main/java/com/ticketing/simulation/ticket_pool_simulation/model/entity/Configuration.java
@@ -0,0 +1,25 @@
+package com.ticketing.simulation.ticket_pool_simulation.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "configurations")
+public class Configuration {
+    @Id
+    private String id;
+    private int totalTickets;
+    private int ticketReleaseRate;  // tickets per minute
+    private int customerRetrievalRate;  // attempts per minute
+    private int maxTicketCapacity;
+    private int vendorCount;
+    private int customerCount;
+    private boolean isRunning;
+}
--- src/main/java/com/ticketing/simulation/ticket_pool_simulation/model/entity/Event.java
+++ src/main/java/com/ticketing/simulation/ticket_pool_simulation/model/entity/Event.java
@@ -1,29 +1,25 @@
 package com.ticketing.simulation.ticket_pool_simulation.model.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Document(collection = "tickets")
-public class Ticket {
+@Document(collection = "events")
+public class Event {
     @Id
     private String id;
-    private String eventId;
+    private String name;
+    private String description;
+    private int totalTickets;
     private double price;
-    private TicketStatus status;
-    private LocalDateTime createdAt;
-    private LocalDateTime purchasedAt;
-    private String purchasedBy;
-
-    public enum TicketStatus {
-        AVAILABLE, PURCHASED
-    }
+    private LocalDateTime eventDate;
 }
